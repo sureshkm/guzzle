1903d87d95a32b2d27e9fa8be0da43255565ffaa
SureshKumar V <sureshv80@gmail.com>
2024-12-30 20:49:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('helloHttp', (req, res) => {
+  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+});
+
